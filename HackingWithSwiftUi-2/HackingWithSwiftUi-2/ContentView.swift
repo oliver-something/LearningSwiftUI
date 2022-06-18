1564f01d561624af1571a43511a315a90c9f8773
Khashayar Hosseini <diablo31@live.co.uk>
2022-06-18 18:14:56 +0430

    New Project Created

---
+++ HackingWithSwiftUi-2/HackingWithSwiftUi-2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HackingWithSwiftUi-2
+//
+//  Created by Oliver Something on 6/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HackingWithSwiftUi-2/HackingWithSwiftUi-2/HackingWithSwiftUi_2App.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWithSwiftUi_2App.swift
+//  HackingWithSwiftUi-2
+//
+//  Created by Oliver Something on 6/18/22.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwiftUi_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
